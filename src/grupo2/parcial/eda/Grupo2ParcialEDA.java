a3bac9c07d854d7aac3d5ab13df54486da8aeda5
Luca Rodrigaño <Lucarodrigano@gmail.com>
2026-06-02 22:41:01 -0300

    Initial commit

---
+++ src/grupo2/parcial/eda/Grupo2ParcialEDA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grupo2.parcial.eda;
+
+/**
+ *
+ * @author Luca Rodrigaño
+ */
+public class Grupo2ParcialEDA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

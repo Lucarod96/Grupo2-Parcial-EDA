55fb15b5c395574c9e1c303539dcc43befa142a9
Alfaro <alfaromilagrosg@gmail.com>
2026-06-04 20:05:12 -0300

    clase Nodo listo

--- src/grupo2/parcial/eda/Nodo.java
+++ src/grupo2/parcial/eda/Nodo.java
@@ -10,5 +10,33 @@ package grupo2.parcial.eda;
  * @author Usuario
  */
 public class Nodo {
-    
+    private Palabra dato;
+    private Nodo izquierdo;
+    private Nodo derecho;
+
+    public Nodo(String palabra) {
+        this.dato = new Palabra(palabra);
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+
+    public Palabra getDato() {
+        return dato;
+    }
+
+    public Nodo getIzquierdo() {
+        return izquierdo;
+    }
+
+    public void setIzquierdo(Nodo izquierdo) {
+        this.izquierdo = izquierdo;
+    }
+
+    public Nodo getDerecho() {
+        return derecho;
+    }
+
+    public void setDerecho(Nodo derecho) {
+        this.derecho = derecho;
+    }
 }
